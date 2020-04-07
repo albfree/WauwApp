714f71d228a3d04ee46cc124d10a68edef6a72bc
dml1994 <dml94@live.com>
2020-04-07 13:32:36 +0200

    refactor - App style
    Arregladas issues de Codacy

--- components/styles/loginStyle.js
+++ components/styles/loginStyle.js
@@ -1,54 +1,58 @@
 import { StyleSheet } from "react-native";
 
+const morado = "#443099";
+const transparente = "transparent";
+const blanco = "#5c54a4";
+
 export const loginStyles = StyleSheet.create({
-  loginImage: {
-    height: "40%",
-    width: "60%",
-    resizeMode: "stretch",
-    alignSelf: "center",
-    marginBottom: "10%",
-    marginTop: "3%",
-  },
   loginBtn: {
-    backgroundColor: "#443099",
+    backgroundColor: morado,
     borderRadius: 30,
-    width: "95%",
     height: "50%",
     justifyContent: "flex-start",
+    width: "95%",
   },
   loginBtnContainer: {
     alignItems: "center",
     alignSelf: "center",
-    width: "75%",
+    backgroundColor: morado,
     height: "10%",
-    backgroundColor: "#443099",
     justifyContent: "center",
-  },
-  loginPrints: {
-    height: "10%",
-    width: "82%",
-    resizeMode: "stretch",
-    backgroundColor: "transparent",
-    alignSelf: "center",
-    marginBottom: "15%",
-    marginTop: "6%",
+    width: "75%",
   },
   loginBtnTittle: {
-    marginLeft: "15%",
     fontSize: 17,
+    marginLeft: "15%",
   },
-  loginView: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 40,
-    paddingBottom: 60,
-    marginBottom: "5%",
+  loginImage: {
+    alignSelf: "center",
+    height: "40%",
+    marginBottom: "10%",
+    marginTop: "3%",
+    resizeMode: "stretch",
+    width: "60%",
+  },
+  loginPrints: {
+    alignSelf: "center",
+    backgroundColor: transparente,
+    height: "10%",
+    marginBottom: "15%",
+    marginTop: "6%",
+    resizeMode: "stretch",
+    width: "82%",
   },
   loginTxt: {
+    color: blanco,
     fontSize: 50,
-    color: "#5c54a4",
     marginTop: "20%",
+  },
+  loginView: {
+    alignItems: "center",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    marginBottom: "5%",
+    paddingTop: 40,
+    paddingBottom: 60,
   },
 });
