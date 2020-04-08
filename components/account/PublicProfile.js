9ebf4f8ed4fddfd2017dcdd9e61c138c8db7e8e7
Alberto Guerrero <albergr1197@gmail.com>
2020-04-08 23:06:19 +0200

    feat - maquetación perfil público

--- components/account/PublicProfile.js
+++ components/account/PublicProfile.js
@@ -3,19 +3,45 @@ import { withNavigation } from "react-navigation";
 import {
     View,
     Text,
-    FlatList,
-    TouchableOpacity,
-    SafeAreaView,
-    Alert
-  } from "react-native";
+    SafeAreaView
+} from "react-native";
+import { Avatar } from "react-native-elements";
+import { globalStyles } from "../styles/global";
+import { ScrollView } from "react-native-gesture-handler";
 
 function PublicProfile(props) {
-    const { navigation } = props;
+    const userInfo = props.navigation.state.params.user;
 
-    return(
-        <View>
-            <Text>HOLA!</Text>
-        </View>
+    //console.log(userInfo);
+
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <View>
+                    <View style={globalStyles.infoUserView}>
+                        <Avatar
+                            rounded
+                            size="large"
+                            containerStyle={globalStyles.userInfoAvatar}
+                            source={{
+                                uri: userInfo.photo
+                            }}
+                        />
+                        <Text style={globalStyles.userInfoDisplayName}>{userInfo.name}</Text>
+                    </View>
+                    <View style={globalStyles.userInfoDescriptionGlobal}>
+                        <Text style={globalStyles.userInfoTitleDescription}>Ubicación</Text>
+                        <Text style={globalStyles.userInfoDescription}>
+                            {userInfo.location}
+                        </Text>
+                        <Text style={globalStyles.userInfoTitleDescription}>Sobre mí</Text>
+                        <Text style={globalStyles.userInfoDescription}>
+                            {userInfo.description}
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
