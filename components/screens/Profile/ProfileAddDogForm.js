03bdc68635868070c2b0aa17767e531af0ee51cd
dml1994 <dml94@live.com>
2020-04-08 03:42:33 +0200

    refactpr - App Style #95
    Revisado CSS del formulario de añadir perros.

--- components/screens/Profile/ProfileAddDogForm.js
+++ components/screens/Profile/ProfileAddDogForm.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, TextInput, Alert, SafeAreaView, Keyboard, ScrollView } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  Alert,
+  SafeAreaView,
+  Keyboard,
+  ScrollView,
+} from "react-native";
 import { db } from "../../population/config.js";
 import { withNavigation } from "react-navigation";
 import { email } from "../../account/QueriesProfile";
 import { Button, Icon } from "react-native-elements";
 import { globalStyles } from "../../styles/global";
+import { addDogStyles } from "../../styles/addDogStyle";
 
 function ProfileAddDogForm(props) {
   const {
@@ -14,7 +23,7 @@ function ProfileAddDogForm(props) {
     description,
     owner,
     setIsVisibleModal,
-    navigation
+    navigation,
   } = props;
   const [newName, setNewName] = useState(null);
   const [newBreed, setNewBreed] = useState(null);
@@ -28,8 +37,7 @@ function ProfileAddDogForm(props) {
     db.ref("wauwers")
       .orderByChild("email")
       .equalTo(email)
-      .on("child_added", snap => {
-        //console.log("snap.val()", snap.val());
+      .on("child_added", (snap) => {
         const newNewOwner = {
           avgScore: snap.val().avgScore,
           description: snap.val().description,
@@ -38,14 +46,12 @@ function ProfileAddDogForm(props) {
           name: snap.val().name,
           photo: snap.val().photo,
           surname: snap.val().surname,
-          wauwPoints: snap.val().wauwPoints
+          wauwPoints: snap.val().wauwPoints,
         };
         setnewOwner(newNewOwner);
-        
       });
     setReloadData(false);
   }, [reloadData]);
-
 
   const addPet = () => {
     let id = db.ref("pet").push().key;
@@ -54,7 +60,7 @@ function ProfileAddDogForm(props) {
       name: newName,
       breed: newBreed,
       description: newDescription,
-      owner: newOwner
+      owner: newOwner,
     };
     var regex = /\w/;
     if (
@@ -85,15 +91,19 @@ function ProfileAddDogForm(props) {
         .set(petData)
         .then(() => {
           let petNumber = 0;
-          db.ref().child("wauwers/" + newOwner.id).on("value", snap => {
-            if(snap.val().petNumber !== undefined) {
-              petNumber = snap.val().petNumber;
-            }
-          });
+          db.ref()
+            .child("wauwers/" + newOwner.id)
+            .on("value", (snap) => {
+              if (snap.val().petNumber !== undefined) {
+                petNumber = snap.val().petNumber;
+              }
+            });
 
-          db.ref().child("wauwers/" + newOwner.id).update({
-            petNumber: petNumber + 1
-          });
+          db.ref()
+            .child("wauwers/" + newOwner.id)
+            .update({
+              petNumber: petNumber + 1,
+            });
 
           Alert.alert("Éxito", "Se ha registrado el perro correctamente.");
           navigation.navigate("ProfileDrawer");
@@ -109,35 +119,33 @@ function ProfileAddDogForm(props) {
   };
 
   return (
-    <SafeAreaView style={globalStyles.safeShowRequestArea}>
+    <SafeAreaView style={globalStyles.viewFlex1}>
       <ScrollView keyboardShouldPersistTaps={false}>
-        <View style={globalStyles.showRequestFeed}>
+        <View style={globalStyles.viewFeed}>
           <View style={globalStyles.viewFlex1}>
-            <Text style={globalStyles.addDogTittle}>
-              ¿Cómo se llama su perro?
-          </Text>
+            <Text style={addDogStyles.addDogTxt}>¿Cómo se llama su perro?</Text>
             <TextInput
-              style={globalStyles.addDogCnt1}
+              style={addDogStyles.addDogTxt2}
               placeholder="Ej.: Fluffy"
-              onChange={v => setNewName(v.nativeEvent.text)}
+              onChange={(v) => setNewName(v.nativeEvent.text)}
             />
-            <Text style={globalStyles.addDogTittle}>¿De qué raza es?</Text>
+            <Text style={addDogStyles.addDogTxt}>¿De qué raza es?</Text>
             <TextInput
-              style={globalStyles.addDogCnt1}
+              style={addDogStyles.addDogTxt2}
               placeholder="Ej.: Chiguagua"
-              onChange={v => setNewBreed(v.nativeEvent.text)}
+              onChange={(v) => setNewBreed(v.nativeEvent.text)}
             />
-            <Text style={globalStyles.addDogTittle}>Describa a su perro</Text>
+            <Text style={addDogStyles.addDogTxt}>Describa a su perro</Text>
             <TextInput
-              style={globalStyles.addDogCnt1}
+              style={addDogStyles.addDogTxt2}
               placeholder="Ej.: Muy manso"
               multiline={true}
               numberOfLines={5}
-              onChange={v => setNewDescription(v.nativeEvent.text)}
+              onChange={(v) => setNewDescription(v.nativeEvent.text)}
             />
             <Button
-              buttonStyle={globalStyles.addDogBtn}
-              containerStyle={globalStyles.addDogBtnContainer}
+              buttonStyle={addDogStyles.addDogBtn}
+              containerStyle={addDogStyles.addDogBtnContainer}
               title="Crear"
               onPress={addPet}
               icon={
@@ -149,7 +157,7 @@ function ProfileAddDogForm(props) {
                   marginLeft={30}
                 />
               }
-              titleStyle={globalStyles.addDogBtnTxt}
+              titleStyle={addDogStyles.addDogBtnTxt}
             />
           </View>
         </View>
---
+++ components/styles/addDogStyle.js
@@ -0,0 +1,47 @@
+import { StyleSheet } from "react-native";
+const verde = "rgba(0,128,0,0.6)";
+const lila = "#d6d6e8";
+
+export const addDogStyles = StyleSheet.create({
+  addDogBtn: {
+    alignSelf: "center",
+    backgroundColor: verde,
+    borderRadius: 30,
+    height: "100%",
+    justifyContent: "flex-start",
+    marginVertical: 10,
+
+    width: "100%",
+  },
+  addDogBtnContainer: {
+    alignSelf: "center",
+    backgroundColor: verde,
+    borderColor: lila,
+    borderWidth: 1,
+    height: 45,
+    justifyContent: "center",
+    marginBottom: 10,
+    marginTop: 10,
+    width: "60%",
+  },
+  addDogBtnTxt: {
+    alignSelf: "center",
+    flex: 1,
+    fontSize: 18,
+    marginRight: 10,
+  },
+  addDogTxt: {
+    alignSelf: "center",
+    fontSize: 18,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  addDogTxt2: {
+    borderRadius: 5,
+    borderColor: "#d6d6e8",
+    borderWidth: 1,
+    textAlign: "center",
+    marginHorizontal: 20,
+    paddingHorizontal: 20,
+  },
+});
