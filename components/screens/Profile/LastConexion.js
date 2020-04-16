36342d61e494a050ee03872dc01fb9a82a7815f3
Pedro <pedrosweng@gmail.com>
2020-04-16 02:05:34 +0200

    feat - listing logins

--- components/screens/Profile/LastConexion.js
+++ components/screens/Profile/LastConexion.js
@@ -1,60 +1,59 @@
 import React,   { useState, useEffect } from "react";
-import { StyleSheet, View, Alert, Text,SafeAreaView,FlatList } from "react-native";
+import { View, Text,SafeAreaView, FlatList } from "react-native";
 import { db } from "../../population/config";
 import { email } from "../../account/QueriesProfile";
 //import { SafeAreaView } from "react-native-safe-area-context";
 import { ScrollView } from "react-native-gesture-handler";
 import BlankView from "../BlankView";
 import Loading from "../../../components/Loading";
+import { fechaParseada } from "./../../utils/DateParser";
 
-export default function LastConexion() {
-  const [logins, setLogins] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [reloadData, setReloadData] = useState(false);
+export default function LastConexion(props) {
+  const { navigation } = props;
+  const [ loginsRegistrados, setLoginsRegistrados ] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [reloadData, setReloadData] = useState([]);
  
-  
-  let idUserUid;
+useEffect(() => {
+  let wauwer;
   db.ref("wauwers")
     .orderByChild("email")
     .equalTo(email)
     .on("child_added", (snap) => {
-      idUserUid = snap.val().userId;
+     wauwer = snap.val();
     });
-
-//nconsole.log(idUserUid);
-
-  useEffect(() => {
+    let i = 0;
+    let logins1 = [];
     db.ref("logins")
       .orderByChild("user")
-      .equalTo(idUserUid)
-      .on("value" , (snap) => {
-        const logins1 = [];
-        snap.forEach((child) => {
-          logins1.push(child.val());
-        });
-       // console.log(logins1);
-        setLogins(logins1);
+      .equalTo(wauwer.userId)
+      .on("child_added" , function (snap){
+        logins1.push(snap.val().fecha);
       });
+      logins1.reverse();
+      let aux = [];
+      for (i = 0 ; i < 10 ; i++){
+        aux.push(logins1[i]);
+      }
+      
+      setLoginsRegistrados(aux);
       setReloadData(false);
       setIsLoading(false);
-  }, [reloadData]);
-
-  //console.log(logins);
-
+  }, []);
+  console.log(loginsRegistrados);
+  console.log("hola");
   return (
     <SafeAreaView>
       <ScrollView>
-        {logins.length>0 ? (
-          console.log("Dentro de la lista"),
-          console.log(logins),
+        {loginsRegistrados.length > 0 ? (
           <FlatList 
-          data = {logins}
+          data = {loginsRegistrados}
           renderItem = {(loginItem) => (
             <View>
-              <Text>{loginItem.fecha}</Text>
+              <Text> { fechaParseada(loginItem.item) } </Text>
             </View>
-          )
-          }
+          )}
+          showsVerticalScrollIndicator={false}
           />
         ) : (
           <BlankView text={"No tiene logins"} />
