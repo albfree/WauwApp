6fd0bb070d317985295136e4c14fd28c802ffb6b
Pedro <pedrosweng@gmail.com>
2020-04-21 09:59:21 +0200

    fix - changing test

--- components/__tests__/screens/pagar.test.js
+++ components/__tests__/screens/pagar.test.js
@@ -1,16 +1,16 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import Pago from "../../screens/Pago";
+import Pagar from "../../screens/Pagar";
 import mockStore from "redux-mock-store";
 
-describe("<Pago />", () => {
+describe("<Pagar />", () => {
   it.skip("testing the payment view", async () => {
     const store = mockStore({
       rehydrated: false,
       navigation: { navigate: jest.fn() },
     });
     const tree = renderer
-      .create(<Pago navigation={store} request={store} />)
+      .create(<Pagar navigation={store} request={store} />)
       .toJSON();
     expect(tree.children.length).toBe(1);
   });
