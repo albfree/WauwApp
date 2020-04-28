6bda4022e872a4ea41d5c26b76412891862b443d
Pedro <pedrosweng@gmail.com>
2020-04-28 21:05:25 +0200

    Revert "fix - inserting popup code in the class"
    This reverts commit 2af7d556cb45213b83e06ffcfd566f191cef83c7.

--- components/screens/Home.js
+++ components/screens/Home.js
@@ -29,23 +29,7 @@ function Home(props) {
 
   useEffect(() => {
     bannedAssertion();
-    let userInfo;
-    db.ref("wauwers")
-    .orderByChild("email")
-    .equalTo(email)
-    .on("child_added", (snap) => {
-      userInfo = snap.val();
-      if (userInfo.hasMessages && userInfo.hasRequests) {
-        Alert.alert("Atención", "Tiene mensajes y notificaciones sin leer");
-      } else if (userInfo.hasRequests) {
-        Alert.alert("Atención", "Tiene notificaciones sin leer");
-      } else if (userInfo.hasMessages) {
-        Alert.alert("Atención", "Tiene mensajes sin leer");
-      }
-    });
-    
-    
-    
+    popUp();
     setReloadData(false);
     setLoading(false);
   }, [reloadData]);
