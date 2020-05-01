72b3129d9a87b61f02d37605d12a42f73722a670
Pablo Borreguero Llorente <borreguerollorente@student.tugraz.at>
2020-05-01 20:19:09 +0200

    refactor - check user null

--- components/screens/DashboardScreen.js
+++ components/screens/DashboardScreen.js
@@ -6,19 +6,30 @@ import firebase from "firebase";
 
 export default function Dashboard(props) {
   const [user, setUserInfo] = useState(props.navigation.state.params.userInfo);
-  
+
   let userInfo = user;
   useEffect(() => {
     db.ref("wauwers")
       .child(userInfo.id)
       .on("value", (snap) => {
-        if (snap.val().isBanned) {
-          Alert.alert("Atención", "Su cuenta ha sido bloqueada.");
-          firebase.auth().signOut();
-        } else if (snap.val().hasRequests || snap.val().hasMessages) {
-          Alert.alert("Atención", "Tiene notificaciones y/o mensajes sin leer");
-        } else {
-          setUserInfo(snap.val());
+        if (snap.val()) {
+          if (snap.val().isBanned) {
+            Alert.alert("Atención", "Su cuenta ha sido bloqueada.");
+            firebase.auth().signOut();
+          } else {
+            if (snap.val().hasRequests && !snap.val().hasMessages) {
+              Alert.alert("Atención", "Tiene notificaciones pendientes");
+            } else if (!snap.val().hasRequests && snap.val().hasMessages) {
+              Alert.alert("Atención", "Tiene mensajes sin leer");
+            } else if (snap.val().hasRequests && snap.val().hasMessages) {
+              Alert.alert(
+                "Atención",
+                "Tiene notificaciones y/o mensajes sin leer"
+              );
+            } else {
+              setUserInfo(snap.val());
+            }
+          }
         }
       });
   }, []);
