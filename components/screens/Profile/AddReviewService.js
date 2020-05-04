dd41aa8211b325752e4f63fe46d8e4dd47f3b36e
Sergio <sergiotb15@gmail.com>
2020-05-04 18:16:54 +0200

    fix - Arreglo precio en el pago al usuario

--- components/screens/Profile/AddReviewService.js
+++ components/screens/Profile/AddReviewService.js
@@ -24,6 +24,7 @@ function AddReviewService(props) {
   const [title, setTitle] = useState("");
   const [review, setReview] = useState("");
   const toastRef = useRef();
+  const salaryRequest = request.salary;
   const [paypalUrl, setPaypalUrl] = useState("");
 
   const uploadData = () => {
@@ -108,7 +109,7 @@ function AddReviewService(props) {
       {
         recipient_type: "EMAIL",
         amount: {
-          value: request.price,
+          value: salaryRequest,
           currency: "EUR",
         },
         note: "Gracias por tu servicio!",
